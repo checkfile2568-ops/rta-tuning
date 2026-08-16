b4616188218393296b0507839d111e6be9667279
chaninposri-blip <chaninposri@gmail.com>
2026-08-16 10:52:17 +0700

    fix(media): self-host ffmpeg worker constants

---
+++ vendor/ffmpeg-0.12.10/const.js
@@ -0,0 +1,23 @@
+// Vendored runtime constants derived from @ffmpeg/ffmpeg v0.12.10 (MIT).
+// Source tag: ffmpegwasm/ffmpeg.wasm v0.12.10
+export const MIME_TYPE_JAVASCRIPT = "text/javascript";
+export const MIME_TYPE_WASM = "application/wasm";
+export const CORE_VERSION = "0.12.6";
+export const CORE_URL = `https://unpkg.com/@ffmpeg/core@${CORE_VERSION}/dist/umd/ffmpeg-core.js`;
+export const FFMessageType = Object.freeze({
+  LOAD: "LOAD",
+  EXEC: "EXEC",
+  WRITE_FILE: "WRITE_FILE",
+  READ_FILE: "READ_FILE",
+  DELETE_FILE: "DELETE_FILE",
+  RENAME: "RENAME",
+  CREATE_DIR: "CREATE_DIR",
+  LIST_DIR: "LIST_DIR",
+  DELETE_DIR: "DELETE_DIR",
+  ERROR: "ERROR",
+  DOWNLOAD: "DOWNLOAD",
+  PROGRESS: "PROGRESS",
+  LOG: "LOG",
+  MOUNT: "MOUNT",
+  UNMOUNT: "UNMOUNT"
+});
