bc71171bd51b838cceb8c32cfe73044fcf677cbf
chaninposri-blip <chaninposri@gmail.com>
2026-09-01 13:00:34 +0700

    Configure PC Remote frontend for new Supabase project

--- pc-remote-control/frontend/assets/config.js
+++ pc-remote-control/frontend/assets/config.js
@@ -1,4 +1,4 @@
 window.PC_REMOTE_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://xxbvqvunimkktztxozvh.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_AhZl7_dcms8f5xSAov4tfQ_Bw_HbrXq"
 };
